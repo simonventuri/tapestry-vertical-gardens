ad10783a998c35487a793ef8982857c4663e32ad
Simon Venturi <simon@southpoolskeptics.com>
2025-08-20 00:44:47 +0100

    Images added and correctly referenced. .gitignore updated.

--- components/Nav.js
+++ components/Nav.js
@@ -1,8 +1,8 @@
 
-export default function Nav(){
+export default function Nav() {
   return (
     <nav className="nav">
-      <a href="/" className="brand">Tapestry Vertical Gardens</a>
+      <a href="/" className="brand"><img src="/images/logo.jpg" alt="Tapestry Vertical Gardens" /></a>
       <div className="links">
         <a href="/about">About</a>
         <a href="/portfolio">Portfolio</a>
